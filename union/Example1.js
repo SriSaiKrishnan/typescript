e1f67b67546290775e8cab1b5af1973b0ffcb67b
Saikrishnan <krishnansai99@gmail.com>
2020-08-30 22:17:15 +0530

    Union Practice

---
+++ union/Example1.js
@@ -0,0 +1,10 @@
+function display(value) {
+    if (typeof (value) == "number") {
+        console.log("The given Numeric Value is " + value);
+    }
+    else if (typeof (value == "string")) {
+        console.log("The given String value is " + value);
+    }
+}
+display(123);
+display("Hello");
---
+++ union/Example2.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var myColor = 'red';
+console.log(myColor.toUpperCase());
