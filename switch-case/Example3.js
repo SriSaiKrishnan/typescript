699e390e6b8f74f3d13ccca80f8ff29c6004ff86
Saikrishnan <krishnansai99@gmail.com>
2020-09-02 22:51:39 +0530

    Decision Making and Switch Case Practice

---
+++ switch-case/Example3.js
@@ -0,0 +1,8 @@
+var number = 20;
+switch (number) {
+    //switch cases without break statements    
+    case 10: console.log("10");
+    case 20: console.log("20");
+    case 30: console.log("30");
+    default: console.log("Not in 10, 20 or 30");
+}
